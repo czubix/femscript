7087c6e493315edd4aa2cd4bf24b8a5e1eb2ee39
czubix <czubek2006@gmail.com>
2024-02-13 16:23:07 +0100

    copyright update

--- cumpile.sh
+++ cumpile.sh
@@ -1,6 +1,7 @@
 PROJECT_NAME="femscript"
+PY_VERSION="3.12"
 
-python3.12 -m maturin build -i python3.12 "$@"
-pip3.12 install --force-reinstall target/wheels/*.whl
+python$PY_VERSION -m maturin build -i python$PY_VERSION "$@"
+pip$PY_VERSION install --force-reinstall target/wheels/*.whl
 
-(echo; echo; cat femscript.py) >> `pip3.12 show $PROJECT_NAME | awk '/Location:/ {print $2}'`/$PROJECT_NAME/__init__.py
+(echo; echo; cat femscript.py) >> $(pip$PY_VERSION show $PROJECT_NAME | awk '/Location:/ {print $2}')/$PROJECT_NAME/__init__.py
